d6c86ff0628d3a15c3a7c6464a6da84786714dde
Douglas Chen <cdswyda@hotmail.com>
2022-11-23 23:47:55 +0800

    add rest and optional for tuple

--- packages/primitive-reference/src/tuple.ts
+++ packages/primitive-reference/src/tuple.ts
@@ -10,4 +10,18 @@ const point4: Point = [0, 10, -10]; // 表示一个三维坐标点
 
 // readonly Tuple Elements
 const originPoint: readonly [number, number] = [0, 0]; // 表示一个二维坐标点
-originPoint[0] = 10; // error: Cannot assign to '0' because it is a read-only property
+// originPoint[0] = 10; // error: Cannot assign to '0' because it is a read-only property
+
+// 可选和剩余
+type Point2DOr3d = [x: number, y: number, z?: number];
+const point5: Point2DOr3d = [0, 10]; // 表示一个二维坐标点
+const point6: Point2DOr3d = [0, 10]; // 表示一个二维坐标点
+point5.length; // (property) length: 2 | 3
+
+type StringNumberBooleans = [string, number, ...boolean[]];
+type StringBooleansNumber = [string, ...boolean[], number];
+type BooleansStringNumber = [...boolean[], string, number];
+
+const arr31: StringNumberBooleans = ["hello",1, true];
+const arr32: StringNumberBooleans = ["beautiful", 2, true];
+const arr33: StringNumberBooleans = ["world", 3, true, false, true, false, true];
